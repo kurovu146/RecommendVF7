6a366b66136f5795dc6f7306823686d3199257e6
kurovu146 <kurobakaito146@gmail.com>
2025-05-26 22:32:27 +0700

    add module players

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
+import { PlayersModule } from './players/players.module';
 
 @Module({
-  imports: [PrismaModule, UsersModule],
+  imports: [PrismaModule, UsersModule, PlayersModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
