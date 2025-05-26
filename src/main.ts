19d49f24fa9672dee5310f53e66d53276848adb8
kurovu146 <kurobakaito146@gmail.com>
2025-05-26 22:56:01 +0700

    remove unuse code

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger, NestApplicationOptions } from '@nestjs/common';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const logger = new Logger();
   const app = await NestFactory.create(AppModule);
-  
+
   await app.listen(process.env.PORT ?? 3000).then(async () => {
     logger.log(`🚀 Server ready at: ${await app.getUrl()} 🚀`);
   });
