18dce3bd28ffadfef09683280f9e8e0fd6fe1b72
kurovu146 <kurobakaito146@gmail.com>
2025-05-26 21:55:12 +0700

    edit prismaService

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,47 +1,18 @@
-import {
-  INestApplication,
-  Injectable,
-  Logger,
-  OnModuleInit,
-} from '@nestjs/common';
+// src/prisma/prisma.service.ts
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
   constructor() {
-    if (process.env.NODE_ENV === 'development') {
-      super({
-        log: [
-          { emit: 'event', level: 'query' },
-          { emit: 'stdout', level: 'info' },
-          { emit: 'stdout', level: 'warn' },
-          { emit: 'stdout', level: 'error' },
-        ],
-        errorFormat: 'colorless',
-      });
-      if (!(process.env.DEBUG === ""))
-        this.enableQueryLogger();
-    } else {
-      super();
-    }
-
+    super({});
   }
 
   async onModuleInit() {
     await this.$connect();
   }
 
-  async enableQueryLogger() {
-    (this.$on as any)('query', (event: any) => {
-      Logger.log(
-        '\nQuery: ' +
-          event.query +
-          '\n**** Params: ' +
-          event.params +
-          '\n**** Duration: ' +
-          event.duration +
-          'ms',
-      );
-    });
+  async onModuleDestroy() {
+    await this.$disconnect();
   }
 }
