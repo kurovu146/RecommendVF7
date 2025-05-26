9a65c8950b05da924dc7afc764b0c92abc3c7ebb
kurovu146 <kurobakaito146@gmail.com>
2025-05-26 22:54:28 +0700

    update log when start

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, NestApplicationOptions } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger();
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  
+  await app.listen(process.env.PORT ?? 3000).then(async () => {
+    logger.log(`🚀 Server ready at: ${await app.getUrl()} 🚀`);
+  });
 }
 bootstrap();
